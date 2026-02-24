3641fe6b8ebd1f0860714acba87cc3c5b3f72507
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2026-02-23 22:33:56 -0500

    Work to save editing interface

--- save_proc.c
+++ save_proc.c
@@ -9,8 +9,12 @@
 #define SAVE_FILE_SIGNATURE  (0x2D472D47)
 #define CHECKSUM_KEY  (0x0000)
 
+#define SAVE_FILE_SIZE  (0x200)
+#define SAVE_ENCODED_SIZE  (0xC0)
+#define SAVE_DECODED_SIZE  (SAVE_ENCODED_SIZE - 0x8)
 
-static void decodeHwords(u16* dst, u16* src, u32 length, u32 key) {
+
+static void decodeHwords(u16* dst, u16* src, u32 length, u16 key) {
 	key ^= 0x4741;
 	for (int i = 0; i < length / 2; i++) {
 		u16 hword = *src++;
@@ -20,7 +24,7 @@ static void decodeHwords(u16* dst, u16* src, u32 length, u32 key) {
 }
 
 
-static void encodeHwords(u16* dst, u16* src, u32 length, u32 key) {
+static void encodeHwords(u16* dst, u16* src, u32 length, u16 key) {
 	key ^= 0x4741;
 	for (int i = 0; i < length / 2; i++) {
 		u16 hword = *src++;
@@ -44,7 +48,182 @@ static u32 generateChecksum(void* save_data, int length) {
 
 
 
-int WD_EncodeSaveData(void* encoded_dst, void* decoded_src) {
+static u32 read32(const u8** datap) {
+	u32 ret = *(*datap)++;
+	ret |= *(*datap)++ << 8;
+	ret |= *(*datap)++ << 16;
+	ret |= *(*datap)++ << 24;
+	return ret;
+}
+
+
+static u16 read16(const u8** datap) {
+	u16 ret = *(*datap)++;
+	ret |= *(*datap)++ << 8;
+	return ret;
+}
+
+
+static u8 read8(const u8** datap) {
+	return *(*datap)++;
+}
+
+
+static void write32(u8** datap, u32 data) {
+	*(*datap)++ = data & 0xFF;
+	*(*datap)++ = (data >> 8) & 0xFF;
+	*(*datap)++ = (data >> 16) & 0xFF;
+	*(*datap)++ = (data >> 24) & 0xFF;
+}
+
+
+static void write16(u8** datap, u16 data) {
+	*(*datap)++ = data & 0xFF;
+	*(*datap)++ = (data >> 8) & 0xFF;
+}
+
+
+static void write8(u8** datap, u8 data) {
+	*(*datap)++ = data;
+}
+
+
+static void deserialize(WDSave* dst, const void* decoded_src) {
+	const u8* src_bytes = (const u8*)decoded_src;
+	
+	for (int i = 0; i < 10; i++) {
+		dst->unlockFlags[i] = read32(&src_bytes);
+	}
+	
+	for (int i = 0; i < 11; i++) {
+		dst->nickname[i] = read16(&src_bytes);
+	}
+	
+	dst->musicVolume = read8(&src_bytes);
+	dst->soundVolume = read8(&src_bytes);
+	
+	dst->travelModeColor = read8(&src_bytes);
+	dst->travelModeAccessory = read8(&src_bytes);
+	dst->travelModeRoom = read8(&src_bytes);
+	
+	dst->dogAge = read8(&src_bytes);
+	dst->dogMood = (s16)read16(&src_bytes);
+	
+	for (int i = 0; i < 4; i++) {
+		dst->dogPersonality[i] = read8(&src_bytes);
+	}
+	
+	dst->dogFriendship = read16(&src_bytes);
+	
+	dst->unk4C = read8(&src_bytes);
+	
+	dst->_unk4D = read8(&src_bytes); // Unknown byte
+	
+	dst->dogHunger = read16(&src_bytes);
+	dst->dogCleanliness = read16(&src_bytes);
+	
+	for (int i = 0; i < 2; i++) {
+		dst->_unk52[i] = read8(&src_bytes); // Unknown 2 bytes
+	}
+	
+	dst->playTime = read32(&src_bytes);
+	
+	for (int i = 0; i < 30; i++) {
+		dst->minigameScores[i] = read16(&src_bytes);
+	}
+	
+	dst->unk94 = read8(&src_bytes);
+	dst->unk95 = read8(&src_bytes);
+	dst->unk96 = read8(&src_bytes);
+	dst->unk97 = read8(&src_bytes);
+	dst->unk98 = read8(&src_bytes);
+	dst->unk99 = read8(&src_bytes);
+	dst->unk9A = read8(&src_bytes);
+	dst->unk9B = read8(&src_bytes);
+	
+	for (int i = 0; i < 2; i++) {
+		dst->_unk9C[i] = read8(&src_bytes); // Unknown 2 bytes
+	}
+	
+	for (int i = 0; i < 21; i++) {
+		dst->unk9E[i] = read8(&src_bytes);
+	}
+	
+	for (int i = 0; i < 6; i++) {
+		dst->_unkB2[i] = read8(&src_bytes); // Unknown 6 bytes
+	}
+}
+
+
+static void serialize(void* decoded_dst, const WDSave* src) {
+	u8* dst_bytes = (u8*)decoded_dst;
+	
+	for (int i = 0; i < 10; i++) {
+		write32(&dst_bytes, src->unlockFlags[i]);
+	}
+	
+	for (int i = 0; i < 11; i++) {
+		write16(&dst_bytes, src->nickname[i]);
+	}
+	
+	write8(&dst_bytes, src->musicVolume);
+	write8(&dst_bytes, src->soundVolume);
+	
+	write8(&dst_bytes, src->travelModeColor);
+	write8(&dst_bytes, src->travelModeAccessory);
+	write8(&dst_bytes, src->travelModeRoom);
+	
+	write8(&dst_bytes, src->dogAge);
+	write16(&dst_bytes, (u16)src->dogMood);
+	
+	for (int i = 0; i < 4; i++) {
+		write8(&dst_bytes, src->dogPersonality[i]);
+	}
+	
+	write16(&dst_bytes, src->dogFriendship);
+	
+	write8(&dst_bytes, src->unk4C);
+	
+	write8(&dst_bytes, src->_unk4D); // Unknown byte
+	
+	write16(&dst_bytes, src->dogHunger);
+	write16(&dst_bytes, src->dogCleanliness);
+	
+	for (int i = 0; i < 2; i++) {
+		write8(&dst_bytes, src->_unk52[i]); // Unknown 2 bytes
+	}
+	
+	write32(&dst_bytes, src->playTime);
+	
+	for (int i = 0; i < 30; i++) {
+		write16(&dst_bytes, src->minigameScores[i]);
+	}
+	
+	write8(&dst_bytes, src->unk94);
+	write8(&dst_bytes, src->unk95);
+	write8(&dst_bytes, src->unk96);
+	write8(&dst_bytes, src->unk97);
+	write8(&dst_bytes, src->unk98);
+	write8(&dst_bytes, src->unk99);
+	write8(&dst_bytes, src->unk9A);
+	write8(&dst_bytes, src->unk9B);
+	
+	for (int i = 0; i < 2; i++) {
+		write8(&dst_bytes, src->_unk9C[i]); // Unknown 2 bytes
+	}
+	
+	for (int i = 0; i < 21; i++) {
+		write8(&dst_bytes, src->unk9E[i]);
+	}
+	
+	for (int i = 0; i < 6; i++) {
+		write8(&dst_bytes, src->_unkB2[i]); // Unknown 6 bytes
+	}
+}
+
+
+
+static void encodeSaveData(void* encoded_dst, void* decoded_src) {
 	u16* dst_hwords = (u16*)encoded_dst;
 	u16* src_hwords = (u16*)decoded_src;
 	
@@ -64,12 +243,10 @@ int WD_EncodeSaveData(void* encoded_dst, void* decoded_src) {
 	checksum_hwords[0] = checksum & 0xFFFF;
 	checksum_hwords[1] = checksum >> 16;
 	encodeHwords(&dst_hwords[2], &checksum_hwords[0], 4, CHECKSUM_KEY);
-	
-	return SAVE_STATUS_OK;
 }
 
 
-int WD_DecodeSaveData(void* decoded_dst, void* encoded_src) {
+static WDSaveStatus decodeSaveData(void* decoded_dst, void* encoded_src) {
 	u16* dst_hwords = (u16*)decoded_dst;
 	u16* src_hwords = (u16*)encoded_src;
 	
@@ -98,33 +275,102 @@ int WD_DecodeSaveData(void* decoded_dst, void* encoded_src) {
 }
 
 
-int WD_ImportSaveFile(void* encoded_dst, FILE* sav_src) {
+
+WDSaveStatus WD_ImportSaveFile(WDSave* dst, const char* savfile_src) {
+	FILE* sav_src = fopen(savfile_src, "rb");
+	if (sav_src == NULL) {
+		return SAVE_STATUS_FILE_FAILURE;
+	}
+	
 	// Ignore padding and backup copy
-	return fread(encoded_dst, 1, SAVE_ENCODED_SIZE, sav_src);
+	u8 encoded_data_buf[SAVE_ENCODED_SIZE];
+	if (fread(&encoded_data_buf[0], 1, SAVE_ENCODED_SIZE, sav_src) != SAVE_ENCODED_SIZE) {
+		fclose(sav_src);
+		return SAVE_STATUS_FILE_FAILURE;
+	}
+	
+	fclose(sav_src);
+	
+	u8 decoded_data_buf[SAVE_DECODED_SIZE];
+	WDSaveStatus status = decodeSaveData(&decoded_data_buf[0], &encoded_data_buf[0]);
+	if (status == SAVE_STATUS_OK) {
+		deserialize(dst, &decoded_data_buf[0]);
+	}
+	
+	return status;
 }
 
 
-int WD_ExportSaveFile(FILE* sav_dst, void* encoded_src) {
+WDSaveStatus WD_ExportSaveFile(const char* savfile_dst, const WDSave* src) {
+	FILE* sav_dst = fopen(savfile_dst, "wb");
+	if (sav_dst == NULL) {
+		return SAVE_STATUS_FILE_FAILURE;
+	}
+	
+	u8 decoded_data_buf[SAVE_DECODED_SIZE];
+	serialize(&decoded_data_buf[0], src);
+	
+	u8 encoded_data_buf[SAVE_ENCODED_SIZE];
+	encodeSaveData(&encoded_data_buf[0], &decoded_data_buf[0]);
+	
+	// Write data, padding, backup data, padding
 	for (int i = 0; i < 2; i++) {
-		fwrite(encoded_src, 1, SAVE_ENCODED_SIZE, sav_dst);
-		int offset = SAVE_ENCODED_SIZE;
+		if (fwrite(encoded_data_buf, 1, SAVE_ENCODED_SIZE, sav_dst) != SAVE_ENCODED_SIZE) {
+			fclose(sav_dst);
+			return SAVE_STATUS_FILE_FAILURE;
+		}
 		
 		u8 fill_byte = 0xFF;
-		while (offset != 0x100) {
-			fwrite(&fill_byte, 1, 1, sav_dst);
-			offset++;
+		for (int offset = SAVE_ENCODED_SIZE; offset != 0x100; offset++) {
+			if (fwrite(&fill_byte, 1, 1, sav_dst) != 1) {
+				fclose(sav_dst);
+				return SAVE_STATUS_FILE_FAILURE;
+			}
 		}
 	}
 	
-	return 0;
+	fclose(sav_dst);
+	
+	return SAVE_STATUS_OK;
 }
 
 
-int WD_ImportDataFile(void* decoded_dst, FILE* bin_src) {
-	return fread(decoded_dst, 1, SAVE_DECODED_SIZE, bin_src);
+
+WDSaveStatus WD_ImportDataFile(WDSave* dst, const char* binfile_src) {
+	FILE* bin_src = fopen(binfile_src, "rb");
+	if (bin_src == NULL) {
+		return SAVE_STATUS_FILE_FAILURE;
+	}
+	
+	u8 decoded_data_buf[SAVE_DECODED_SIZE];
+	if (fread(&decoded_data_buf[0], 1, SAVE_DECODED_SIZE, bin_src) != SAVE_DECODED_SIZE) {
+		fclose(bin_src);
+		return SAVE_STATUS_FILE_FAILURE;
+	}
+	
+	fclose(bin_src);
+	
+	deserialize(dst, &decoded_data_buf[0]);
+	
+	return SAVE_STATUS_OK;
 }
 
 
-int WD_ExportDataFile(FILE* bin_dst, void* decoded_src) {
-	return fwrite(decoded_src, 1, SAVE_DECODED_SIZE, bin_dst);
+WDSaveStatus WD_ExportDataFile(const char* binfile_dst, const WDSave* src) {
+	FILE* bin_dst = fopen(binfile_dst, "wb");
+	if (bin_dst == NULL) {
+		return SAVE_STATUS_FILE_FAILURE;
+	}
+	
+	u8 decoded_data_buf[SAVE_DECODED_SIZE];
+	serialize(&decoded_data_buf[0], src);
+	
+	if (fwrite(&decoded_data_buf[0], 1, SAVE_DECODED_SIZE, bin_dst) != SAVE_DECODED_SIZE) {
+		fclose(bin_dst);
+		return SAVE_STATUS_FILE_FAILURE;
+	}
+	
+	fclose(bin_dst);
+	
+	return SAVE_STATUS_OK;
 }
--- save_proc.h
+++ save_proc.h
@@ -2,24 +2,140 @@
 #define SAVE_PROC_H
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-#define SAVE_FILE_SIZE  (0x200)
-#define SAVE_ENCODED_SIZE  (0xC0)
-#define SAVE_DECODED_SIZE  (SAVE_ENCODED_SIZE - 0x8)
 
-enum {
+typedef enum {
 	SAVE_STATUS_OK,
 	SAVE_STATUS_SIGNATURE_MISMATCH,
-	SAVE_STATUS_CHECKSUM_MISMATCH
+	SAVE_STATUS_CHECKSUM_MISMATCH,
+	
+	SAVE_STATUS_FILE_FAILURE
+} WDSaveStatus;
+
+typedef enum {
+	// TODO all unlock flags
+	UNLOCK_TODO
+} WDUnlock;
+
+static const char* WDUnlockStr[] = {
+	// TODO all unlocks by name
 };
 
-int WD_EncodeSaveData(void* encoded_dst, void* decoded_src);
-int WD_DecodeSaveData(void* decoded_dst, void* encoded_src);
+enum WDColor {
+	COLOR_BLUE,
+	COLOR_GREEN,
+	COLOR_RED,
+	COLOR_PINK,
+	COLOR_PURPLE,
+	COLOR_YELLOW
+};
 
-int WD_ImportSaveFile(void* encoded_dst, FILE* sav_src);
-int WD_ExportSaveFile(FILE* sav_dst, void* encoded_src);
+static const char* WDColorStr[] = {
+	"Sky Blue",
+	"Green", // TODO proper names for the rest
+	"Red",
+	"Pink",
+	"Purple",
+	"Yellow"
+};
 
-int WD_ImportDataFile(void* decoded_dst, FILE* bin_src);
-int WD_ExportDataFile(FILE* bin_dst, void* decoded_src);
+enum WDAccessory {
+	// TODO all accessories by ID
+	ACCESSORY_TODO
+};
+
+static const char* WDAccessoryStr[] = {
+	// TODO all accessories by name
+};
+
+enum WDRoom {
+	ROOM_LIVING_ROOM,
+	ROOM_GARDEN,
+	ROOM_BEDROOM, 
+	ROOM_KITCHEN,
+	ROOM_ROOFTOP
+};
+
+static const char* WDRoomStr[] = {
+	"Living Room",
+	"Garden",
+	"Bedroom",
+	"Kitchen",
+	"Rooftop"
+};
+
+enum WDPersonalities {
+	PERSONALITY_NAUGHTY,
+	PERSONALITY_CHEERFUL,
+	PERSONALITY_BIG_EATER,
+	PERSONALITY_NEAT_FREAK
+};
+
+enum WDMiniGame {
+	// TODO minigames by index
+	MINIGAME_TODO
+};
+
+static const char* WDMiniGameStr[] = {
+	// TODO minigames by name
+};
+
+enum WDMiniGameDifficulty {
+	DIFFICULTY_EASY,
+	DIFFICULTY_HARD
+};
+
+typedef struct {
+	uint32_t    unlockFlags[10];        // see other doc for offsets to unlock
+	uint16_t    nickname[11];           // 10 wchars + null terminator
+	uint8_t     musicVolume;            // 0-127
+	uint8_t     soundVolume;            // 0-127
+	uint8_t     travelModeColor;        // 0-5, blue/green/red/pink/purple/yellow
+	uint8_t     travelModeAccessory;    // TODO document enums
+	uint8_t     travelModeRoom;         // TODO document enums
+	uint8_t     dogAge;                 // 1-10
+	int16_t     dogMood;                // -128-127
+	uint8_t     dogPersonality[4];      // scores: [0]=naughty, [1]=cheerful, [2]=big eater, [3]=neat freak
+	uint16_t    dogFriendship;          // 0-54999
+	uint8_t     unk4C;                  // (compared to a random number every few seconds)
+	uint8_t     _unk4D;                 //<! not accessed?
+	uint16_t    dogHunger;              // 0=hungry, drains over time, foods add to it
+	uint16_t    dogCleanliness;         // 0-251, grooming increases
+	uint8_t     _unk52[2];              //<! not accessed?
+	uint32_t    playTime;               // measured in frames, max 268435456
+	uint16_t    minigameScores[30];     // TODO figure out array order, 2x3x5
+	uint8_t     unk94;                  // possibly an array, purpose unknown
+	uint8_t     unk95;                  // *
+	uint8_t     unk96;                  // *
+	uint8_t     unk97;                  // *
+	uint8_t     unk98;                  // *
+	uint8_t     unk99;                  // *
+	uint8_t     unk9A;                  // *
+	uint8_t     unk9B;                  // *
+	uint8_t     _unk9C[2];              //<! not accessed?
+	uint8_t     unk9E[21];              // array accessed, purpose unknown
+	uint8_t     _unkB2[6];              //<! not accessed?
+} WDSave;
+
+WDSaveStatus WD_ImportSaveFile(WDSave* dst, const char* savfile_src);
+WDSaveStatus WD_ExportSaveFile(const char* savfile_dst, const WDSave* src);
+
+WDSaveStatus WD_ImportDataFile(WDSave* dst, const char* binfile_src);
+WDSaveStatus WD_ExportDataFile(const char* binfile_dst, const WDSave* src);
+
+static inline bool WD_IsUnlocked(WDSave* sav, WDUnlock unlockable) {
+	return sav->unlockFlags[unlockable >> 5] & (1 << (unlockable & 31));
+}
+
+static inline void WD_SetUnlocked(WDSave* sav, WDUnlock unlockable, bool unlocked) {
+	uint32_t bit = 1 << (unlockable & 31);
+	if (unlocked) {
+		sav->unlockFlags[unlockable >> 5] |= bit;
+	} else {
+		sav->unlockFlags[unlockable >> 5] &= ~bit;
+	}
+}
 
 #endif
